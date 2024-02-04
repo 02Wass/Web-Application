8644e525d8e02a30251ef62f4146357bc04a3b39
02Wass <123019182+02Wass@users.noreply.github.com>
2024-02-04 01:31:18 +0100

    added register controller

--- hello/src/main/java/com/Wass/webapp/controller/HelloController.java
+++ hello/src/main/java/com/Wass/webapp/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.webapp.Wass;
+package com.Wass.webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
-
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
